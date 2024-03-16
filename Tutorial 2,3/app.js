55ff0bfe0c6c44685d00514c872a91246da6b3a9
Devottam Vaishnav <81460876+devottam123@users.noreply.github.com>
2024-03-16 11:05:07 +0000

    Day 1

---
+++ Tutorial 2,3/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#vue-app",
+    data: {
+        name: "Shaun",
+        job: "Ninja"
+    },
+    methods:{
+        greet: function (time){
+            return 'Good '+ time + ' ' + this.name;
+        }
+    }
+    
+});
